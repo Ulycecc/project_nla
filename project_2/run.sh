8ec8b2a2d69eb51fc8e8da45b49281db4a6bfd24
kbrodt <cyrill.brodt@gmail.com>
2023-12-25 09:58:42 -0500

    Create run.sh for experiments

---
+++ project_2/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+
+python tt.py --dim 1 --data-path ./data/BOE-XUDLNKG.csv --is-random
+python tt.py --dim 1 --data-path ./data/BOE-XUDLNKG.csv
+
+python tt.py --dim 2 --data-path ./data/1703282586.png --use-img-dim
+python tt.py --dim 2 --data-path ./data/1703282586.png --is-random
+python tt.py --dim 2 --data-path ./data/1703282586.png
